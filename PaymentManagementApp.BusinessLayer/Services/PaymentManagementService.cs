c219b7df36ab41097484db331f323fd5309d404a
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-11-27 13:13:54 +0530

    Add files via upload

---
+++ PaymentManagementApp.BusinessLayer/Services/PaymentManagementService.cs
@@ -0,0 +1,46 @@
+﻿using PaymentManagementApp.BusinessLayer.Interfaces;
+using PaymentManagementApp.BusinessLayer.Services.Repository;
+using PaymentManagementApp.BusinessLayer.ViewModels;
+using PaymentManagementApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentManagementApp.BusinessLayer.Services
+{
+    public class PaymentManagementService : IPaymentManagementService
+    {
+        private readonly IPaymentManagementRepository _repo;
+
+        public PaymentManagementService(IPaymentManagementRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<Payment> CreatePayment(Payment employeePayment)
+        {
+            return await _repo.CreatePayment(employeePayment);
+        }
+
+        public async Task<bool> DeletePaymentById(long id)
+        {
+            return await _repo.DeletePaymentById(id);
+        }
+
+        public List<Payment> GetAllPayments()
+        {
+            return  _repo.GetAllPayments();
+        }
+
+        public async Task<Payment> GetPaymentById(long id)
+        {
+            return await _repo.GetPaymentById(id);
+        }
+
+        public async Task<Payment> UpdatePayment(PaymentViewModel model)
+        {
+           return await _repo.UpdatePayment(model);
+        }
+    }
+}
---
+++ PaymentManagementApp.BusinessLayer/Services/Repository/PaymentManagementRepository.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentManagementApp.BusinessLayer.ViewModels;
+using PaymentManagementApp.DataLayer;
+using PaymentManagementApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PaymentManagementApp.BusinessLayer.Services.Repository
+{
+    public class PaymentManagementRepository : IPaymentManagementRepository
+    {
+        private readonly PaymentManagementAppDbContext _dbContext;
+        public PaymentManagementRepository(PaymentManagementAppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Payment> CreatePayment(Payment PaymentModel)
+        {
+            try
+            {
+                var result = await _dbContext.Payments.AddAsync(PaymentModel);
+                await _dbContext.SaveChangesAsync();
+                return PaymentModel;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public async Task<bool> DeletePaymentById(long id)
+        {
+            try
+            {
+                _dbContext.Remove(_dbContext.Payments.Single(a => a.PaymentId== id));
+                _dbContext.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public List<Payment> GetAllPayments()
+        {
+            try
+            {
+                var result = _dbContext.Payments.
+                OrderByDescending(x => x.PaymentId).Take(10).ToList();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public async Task<Payment> GetPaymentById(long id)
+        {
+            try
+            {
+                return await _dbContext.Payments.FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+       
+        public async Task<Payment> UpdatePayment(PaymentViewModel model)
+        {
+            var Payment = await _dbContext.Payments.FindAsync(model.PaymentId);
+            try
+            {
+
+                _dbContext.Payments.Update(Payment);
+                await _dbContext.SaveChangesAsync();
+                return Payment;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+    }
+}
